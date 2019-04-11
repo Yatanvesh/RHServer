8df3ad06b4ff75e30e6b6a3919b6379648598a03
yatanvesh <yatan.vesh@gmail.com>
2019-04-11 21:35:17 +0530

    test3

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const knex = require('knex');
 const enableWs = require('express-ws');
 const app = express();
 enableWs(app);
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
 let successObject = {
   response:"arre londeee"
