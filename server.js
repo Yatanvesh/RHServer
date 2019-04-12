14849a24368d923a987ca9d628e1ef752882b289
yatanvesh <yatan.vesh@gmail.com>
2019-04-12 12:13:26 +0530

    wowoww

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const enableWs = require('express-ws');
 const app = express();
 enableWs(app);
 app.use(bodyParser.json());
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'postgresql://postgresql:5432/',
+        user: "userRW3",
+        password: 'SoroQ2jQtYbCCHwU',
+        database: 'exigency'
+    }
+});
 // app.use(cors());
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
@@ -19,12 +29,46 @@ app.get('/', (req, res) => {
     res.send("success");
 });
 
+let localUser ={
+  email:'a',
+  password:'a'
+};
+
 
 app.ws('/user', (ws, req) => {
 
   ws.on('message', msg => {
-    console.log(msg);
-      ws.send(JSON.stringify(successObject));
+    const {
+        action
+    } = req.body;
+    switch(action){
+      case 'signin':
+      const {email, password} = req.body;
+      if (email == localUser.email && password == localUser.password){
+        res.json({
+          action:signinResonse,
+          status:'success',
+          name:"Gawaar",
+          details:"empty for now"
+        })
+      }
+      else res.json({
+        action:signinResonse,
+        status:'fail',
+      })
+      // db.select('*').from('users').where({
+      //     email: email
+      // }).then(user => {
+      //     console.log('user quer', user);
+      //      if (user.length)
+      //          res.json(user[0]);
+      //      else res.status(400).json('user not found')
+      //})
+
+      break;
+      default:console.log('unkonwn action', action);
+    }
+
   })
 
   ws.on('close', () => {
